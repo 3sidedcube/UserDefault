3cbaba24001d92ef177bd8f52365196fcba8d7fb
Marliza <marliza@3sidedcube.com>
2023-07-31 15:20:49 +0100

    update swift version and platforms

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,7 +7,7 @@ let package = Package(
     name: "UserDefault",
     platforms: [
         .macOS(.v11),
-        .iOS(.v14)
+        .iOS(.v15)
     ],
     products: [
         .library(
